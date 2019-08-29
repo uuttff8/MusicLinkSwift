d8d6684447333e906fe839e7c26f15a61600fe19
uuttff8 <sassanid18@gmail.com>
2019-08-29 18:08:39 +0300

    Initial Commit

---
+++ Music Link/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Music Link
+//
+//  Created by uuttff8 on 8/29/19.
+//  Copyright © 2019 uuttff8. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
