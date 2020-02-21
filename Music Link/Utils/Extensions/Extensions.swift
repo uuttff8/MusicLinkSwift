ae74a73b34ecb961ad2c14e75ed9e7ea9dcb5eb5
Anton Kuzmin <sassanid18@gmail.com>
2020-02-21 19:08:50 +0300

    to be fix

--- Music Link/Utils/Extensions/Extensions.swift
+++ Music Link/Utils/Extensions/Extensions.swift
@@ -32,7 +32,7 @@ extension UIApplication {
     // check for is first launch - only true on first invocation after app install, false on all further invocations
     // Note: Store this value in AppDelegate if you have multiple places where you are checking for this flag
     static func isFirstLaunch() -> Bool {
-        let hasBeenLaunchedBeforeFlag = "hasBeenLaunchedBeforeFlag"
+        let hasBeenLaunchedBeforeFlag = "hasBeenLaunchedBeaaforeFlag"
         let isFirstLaunch = !UserDefaults.standard.bool(forKey: hasBeenLaunchedBeforeFlag)
         if (isFirstLaunch) {
             UserDefaults.standard.set(true, forKey: hasBeenLaunchedBeforeFlag)
